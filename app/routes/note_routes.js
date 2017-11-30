9d1d9e11094b61fa07a92faf289a580dd3737eb9
Jason <jason.dubreuil@yahoo.com>
2017-11-30 10:33:34 -0800

    SET
    SET

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,10 @@
 module.exports = function(app, db) {
+  app.post('/notes', (req, res) => {
+    // You'll create your note here.
+    console.log('hit this section of code');
+    console.log(req.body);
+    console.log(req.title);
 
-  
+    res.send('Hello')
+  });
 };
